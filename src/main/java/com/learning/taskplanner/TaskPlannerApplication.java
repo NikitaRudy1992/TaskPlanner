0b8fcfbdc299a2892b7f10d1808e44c54e938f2b
NikitaRudy1992 <rudyairsoft1992@gmail.com>
2023-11-23 15:10:28 +0100

    Initial commit

---
+++ src/main/java/com/learning/taskplanner/TaskPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.learning.taskplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/taskplanner/TaskPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.taskplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
